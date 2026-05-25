2427b2a93102e828238784cee6025e59d421a0ab
Fatih-L200240158 <l200240158@student.ums.ac.id>
2026-05-25 23:45:14 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,15 +1,24 @@
 import os
 from dotenv import load_dotenv
 
-# Membaca file .env jika ada (terutama saat kamu running di laptop)
 load_dotenv()
 
 class Config:
-    # Kunci rahasia untuk mengamankan data Session / Cookie Login
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci_rahasia_sewa_kamera_2024'
     
-    # Konfigurasi database dinamis (Membaca Docker di laptop, atau membaca Clever Cloud di Render)
-    MYSQL_HOST = os.environ.get('DB_HOST') or 'localhost'
-    MYSQL_USER = os.environ.get('DB_USER') or 'root'
-    MYSQL_PASSWORD = os.environ.get('DB_PASSWORD') or 'rahasia_kamera'
-    MYSQL_DB = os.environ.get('DB_NAME') or 'db_sewa_kamera'
+    # Biarkan tetap membaca os.environ seperti ini!
+    HOST = os.environ.get('DB_HOST') or 'localhost'
+    USER = os.environ.get('DB_USER') or 'root'
+    PASSWORD = os.environ.get('DB_PASSWORD') or 'rahasia_kamera'
+    DB = os.environ.get('DB_NAME') or 'db_sewa_kamera'
+
+    # Ini WAJIB dimasukkan agar semua jenis library Flask MySQL bisa membaca datanya
+    MYSQL_HOST = HOST
+    MYSQL_USER = USER
+    MYSQL_PASSWORD = PASSWORD
+    MYSQL_DB = DB
+
+    MYSQL_DATABASE_HOST = HOST
+    MYSQL_DATABASE_USER = USER
+    MYSQL_DATABASE_PASSWORD = PASSWORD
+    MYSQL_DATABASE_DB = DB
